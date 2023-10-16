08235559ce7a0a895d69eb028602969e3633a52b
kyj94 <willbackv@gmail.com>
2023-10-16 16:34:10 +0900

    🎉Start!

---
+++ src/main/java/com/greenapple/greenapple/GreenappleApplication.java
@@ -0,0 +1,13 @@
+package com.greenapple.greenapple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenappleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenappleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenapple/greenapple/GreenappleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenapple.greenapple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenappleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
